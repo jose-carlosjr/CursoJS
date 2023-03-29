7918544fccc26d14e852ac5a47adefb477391460
José Carlos <josecarlos099916@gmail.com>
2023-03-29 12:25:40 -0300

    do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+/*
+var c = 1
+
+while (c <= 5) {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+var c = 1
+
+do {
+    console.log(`passo ${c}`)
+    c++
+}
+while (c <= 5)
---
+++ aula13/node_70c8585751b4e.tmp.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+
+while (c <= 5) {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+var c = 1
+
+do {
+    console.log(`passo ${c}`)
+}
+while (c <= 5)
